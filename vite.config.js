cfae575ac243258f24682adaa1f2d0e6405269c4
reito <cnschwarzer@qq.com>
2023-03-23 12:10:44 +0800

    save

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,17 @@ import { defineConfig } from "vite";
 import Vue from "@vitejs/plugin-vue"; 
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    Vue() 
-  ],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(({ command, mode }) => { 
+  console.log('Building for', mode);
+  return { 
+    plugins: [
+      Vue() 
+    ],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
+    base: mode === "github-pages" ? '/MarkdownRenderer/' : '/'
+  }
 });
