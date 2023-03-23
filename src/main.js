785f9f86e2d8214a87020e4a80ec1fc0b13b61e7
reito <cnschwarzer@qq.com>
2023-03-23 12:55:48 +0800

    save

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ import App from "./App.vue";
 import Markdown from "./Markdown.vue";
 
 import "./main.css";
-import "highlight.js/styles/panda-syntax-dark.css";
+import "highlight.js/styles/panda-syntax-dark.css"; 
 
+console.log(import.meta.env.BASE_URL)
 const app = createApp(App);
 app.use(
   createRouter({
-    history: createWebHistory(),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
-      { path: "/", component: Markdown },
-      { path: "/MarkdownRenderer", component: Markdown },
+      { path: "/", component: Markdown }, 
     ],
   })
 );
